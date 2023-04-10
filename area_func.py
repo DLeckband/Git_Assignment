bc35edc53cc085cd2e54f39318e5572bce3594e9
DLeckband <130327562+DLeckband@users.noreply.github.com>
2023-04-10 00:20:53 -0700

    Add files via upload

---
+++ area_func.py
@@ -0,0 +1,6 @@
+length = int(input("Enter an integer for the rectangle's length:"))
+width = int(input("Enter an integer for the rectangle's width:"))
+height = int(input("Enter an integer for the rectangle's height:"))
+def rect_area(length, width, height):
+    area = (length * width * height)
+    return area
---
+++ solid_area_func.py
@@ -0,0 +1,7 @@
+length = int(input("Enter an integer for the rectangle's length:"))
+width = int(input("Enter an integer for the rectangle's width:"))
+height = int(input("Enter an integer for the rectangle's height:"))
+def rect_solid_area(length, width, height):
+    solid_area = ((rect_area(length, width, 1) * 2) + (rect_area(length, 1, height) * 2) +
+                  (rect_area(1, width, height) * 2))
+    return print(solid_area)
